7ff8d60a34af9ef3b3752362d92b29982c517438
mo13226785330 <1154670797@qq.com>
2019-05-25 12:26:32 +0800

    123

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'test',
+    'database'        => 'test_tp',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'test_tp',
     // 密码
-    'password'        => 'root',
+    'password'        => 'h6dYbZ83FkDrds6s',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
