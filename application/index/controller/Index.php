cd19a16c35cec2538336c945ec22c8536dae13cf
mo13226785330 <1154670797@qq.com>
2019-05-25 11:03:24 +0800

    测试

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,20 +8,20 @@ class Index
 {
     public function index()
     {
-        $redis = new Redis();
-        $redis->connect('127.0.0.1', 6379);
-        $sql = "select id, uid, agent_id, test from test  limit 100000,100000";
-        $list = Db::query($sql);
-//        dump($list);die;
-        foreach ($list as $v){
-            $redis->hSet('user-between-uid-id',$v['uid'],$v['id']);
-            $redis->hSet('user-between-id-uid',$v['id'],$v['uid']);
-//            if (empty($v['agent_id'])){
-//                $v['agent_id'] = 0;
-//            }
-            $redis->hSet('user-between-agent-id',$v['id'],$v['agent_id']);
-            $redis->hSet('user-between-id-test',$v['id'],$v['test']);
-        }
+//        $redis = new Redis();
+//        $redis->connect('127.0.0.1', 6379);
+//        $sql = "select id, uid, agent_id, test from test  limit 100000,100000";
+//        $list = Db::query($sql);
+////        dump($list);die;
+//        foreach ($list as $v){
+//            $redis->hSet('user-between-uid-id',$v['uid'],$v['id']);
+//            $redis->hSet('user-between-id-uid',$v['id'],$v['uid']);
+////            if (empty($v['agent_id'])){
+////                $v['agent_id'] = 0;
+////            }
+//            $redis->hSet('user-between-agent-id',$v['id'],$v['agent_id']);
+//            $redis->hSet('user-between-id-test',$v['id'],$v['test']);
+//        }
         echo 'OK';
 
     }
